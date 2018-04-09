5ed4cbc872bdfe61cad0a256f8dff7b40095f49c
Gautam Sridhar <gautam.neon@gmail.com>
2018-04-09 11:46:45 +0200

    Add files via upload

---
+++ convtopy.py
@@ -0,0 +1,200 @@
+import os
+import torch
+import pandas as pd
+from skimage import io, transform,img_as_float
+import numpy as np
+from torch.utils.data import Dataset,DataLoader
+from torchvision import transforms, utils
+import time
+
+import warnings
+
+#import matplotlib.pyplot as plt
+
+warnings.filterwarnings("ignore")
+
+root_dir = '/home/sridharg/Documents/Biomed_Imaging/BioMed_Imaging/Dataset/DATA/'
+
+names = pd.read_csv(root_dir + 'names.csv')
+
+class MiceDataset(Dataset):
+	"""Mice dataset loading"""
+	def __init__(self,csv_file,root_dir,transform=None):
+		"""
+		Args:
+				csv_file(string): Path to the csv file with annotations
+				root_dir(string): Directory with all the images
+				transform(callable,optional): Optional transform to be applied on a sample
+		"""
+		self.names = names
+		self.root_dir = root_dir
+		self.transform = transform
+		
+	def __len__(self):
+		return len(self.names)
+		
+	def __getitem__(self, idx):
+		img_name = os.path.join(self.root_dir,self.names.iloc[idx,0])
+		image = img_as_float(io.imread(img_name))
+
+		gtruth_name = os.path.join(self.root_dir,self.names.iloc[idx,1])
+		gtruths = io.imread(gtruth_name)
+		
+		sample = {'image':image, 'gtruths':gtruths}
+		
+		if self.transform:
+			sample = self.transform(sample)
+		
+		return sample
+
+class Rescale(object):
+	""" Rescale the image in a sample to a given size.
+	Args:
+		 Output_size(tuple or int): Desired output size. If tuple, output is matched to output_size. 
+		 If int, smaller of image edges is matched to output_size keeping aspect ratio the same
+	"""
+	
+	def __init__(self,output_size):
+		assert isinstance(output_size,(int,tuple))
+		self.output_size = output_size
+		
+	def __call__(self,sample):
+		image,gtruths = sample['image'], sample['gtruths']
+		
+		h,w = image.shape[:2]
+		if isinstance(self.output_size, int):
+			if h>w:
+				new_h, new_w = self.output_size*w/h, self.output_size
+			else:
+				new_h, new_w = self.output_size,self.output_size
+		else:
+			new_h, new_w = self.output_size
+		
+		new_h, new_w = int(new_h),int(new_w)
+		
+		img = img_as_ubyte(transform.resize(image,(new_h,new_w)))
+		
+		gtrut = img_as_ubyte(transform.resize(gtruths,(new_h,new_w)))
+		
+		return {'image':img, 'gtruths':gtrut}
+		
+class RandomCrop(object):
+	""" Crop randomly the image in a sample
+	Args:
+		output_size(tuple or int): Desired output size. if int, square crop
+	"""
+	def __init__(self,output_size):
+		assert isinstance(output_size, (int,tuple))
+		if isinstance(output_size,int):
+			self.output_size = (output_size, output_size)
+		else:
+			assert len(output_size) == 2
+			self.output_size = output_size
+			
+	def __call__(self,sample):
+		image,gtruths = sample['image'],sample['gtruths']
+		
+		h,w = image.shape[:2]
+		new_h, new_w = self.output_size
+		
+		left = np.random.randint(20, w-new_w)
+		
+		image = image[40:40 + new_h, left:left+new_w]
+		gtruths = gtruths[40:40 + new_h, left:left+new_w]
+		
+		return{'image':image, 'gtruths':gtruths}
+		
+class RandomFlipVertical(object):
+	""" Flip an image randomly on its horizontal axis
+	Args:
+		probability p :  probability of the flip being carried out. Default p = 0.5
+	"""
+	def __init__(self,p = None):
+		if p is None:
+			self.p = 0.5
+		else:
+			self.p = p
+	
+	def __call__(self,sample):
+		image,gtruths = sample['image'],sample['gtruths']
+		a = np.random.uniform()
+		if (a <= self.p):
+			im = np.zeros((image.shape))
+			gt = np.zeros((image.shape))
+			for j in range(image.shape[1]):
+				for i in range(image.shape[0]):
+					im[i,j] = image[image.shape[0]-1-i,j]
+					gt[i,j] = gtruths[image.shape[0]-1-i,j]
+		else:
+			im = image 
+			gt = gtruths 
+			
+		return {'image':im, 'gtruths':gt}
+		
+class RandomFlipHorizontal(object):
+	""" FLip an image randomly on its vertical
+	Args:
+		probability p : probability of the flip being carried out. Default p = 0.5
+	"""
+	def __init__(self,p = None):
+		if p is None:
+			self.p = 0.5
+		else:
+			self.p = p
+	def __call__(self,sample):
+		image,gtruths = sample['image'],sample['gtruths']
+		a = np.random.uniform()
+		if (a <= self.p):
+			im = np.zeros((image.shape))
+			gt = np.zeros((image.shape))
+			for i in range(image.shape[0]):
+				for j in range(image.shape[1]):
+					im[i,j] = image[i,image.shape[1]-1-j]
+					gt[i,j] = gtruths[i,gtruths.shape[1]-j-1]
+		else:
+			im = image
+			gt = gtruths	
+		return {'image':im, 'gtruths':gt}
+		
+class ToTensor(object):
+	""" Convert ndarrays in sample to Tensors, convert the gtruth arrays to required format"""
+	
+	def __call__(self,sample):
+		image,gtruths = sample['image'] , sample['gtruths']
+		#image = (image - 0.09444)/0.2270
+		image = (image - 0.09444)
+		#img = np.zeros((3,image.shape[0],image.shape[1]))
+		#img[0,:,:] = image
+		#img[1,:,:] = image
+		#img[2,:,:] = image
+		image = np.expand_dims(image,axis=0)
+		c_size = 11
+		h,w = gtruths.shape[:2]
+		gt_new = np.zeros((c_size,h,w), dtype = np.uint8)
+		for i in range(c_size):
+			idx = np.where(gtruths == i+1)
+			idx_x = idx[0]
+			idx_y = idx[1]
+			for j in range(idx_x.shape[0]):
+				gt_new[i,idx_x[j],idx_y[j]] = 1
+			
+		#for j in range(gtruths.shape[0]):
+			#for k in range(gtruths.shape[1]):
+				#if gtruths[j,k] == 1:
+						#gt_new[0,j,k] = 255
+							
+		#if self.mode == "Val":
+			#gt_new = gtruths			
+		return {'image':torch.from_numpy(image).double(),'gtruths':torch.from_numpy(gt_new).double()}
+		#return {'image':image,'gtruths':gt_new}
+"""		
+if __name__ == "__main__":
+	
+	dataset_train = MiceDataset(csv_file=names,root_dir=root_dir,transform = transforms.Compose([RandomCrop((224,224)),RandomFlipHorizontal(),ToTensor(mode="Train")]))
+	
+	for i in range(45):
+		sample = dataset_train[i]
+		print(i)
+		plt.imshow(sample['image'][0,:,:], cmap = 'gray')
+		plt.show()
+"""		
---
+++ train.py
@@ -0,0 +1,184 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms,utils,models
+
+import FCN8_model as fcn
+import convgg as Dat
+
+from PIL import Image
+
+import numpy as np
+import pandas as pd
+import time
+import sys
+import os
+
+from tensorboard_logger import configure, log_value
+
+#import matplotlib.pyplot as plt
+
+#os.environ["CUDA_VISIBLE_DEVICES"]= 4
+
+configure("runs/run_9",flush_secs=5)
+
+model_dir = "/home/sridharg/Documents/Biomed_Imaging/Saved_models/"
+
+n_class = 11
+
+batch_size = 5
+epochs = 5001
+lr = 1e-4
+momentum = 0
+w_decay = 0
+step_size = 15
+gamma = 0.5
+
+use_gpu = torch.cuda.is_available()
+
+#need to load the data
+root_dir = '/home/sridharg/Documents/Biomed_Imaging/BioMed_Imaging/Dataset/DATA/'
+
+names = pd.read_csv(root_dir + 'names.csv')
+dataset_train = Dat.MiceDataset(csv_file=names,root_dir=root_dir,transform=transforms.Compose([Dat.RandomCrop((224,224)),Dat.RandomFlipHorizontal(),Dat.ToTensor()]))
+
+#sample = dataset_train[3]
+#print(3, sample['image'].size(), sample['gtruths'].size())
+#plt.imshow(sample['image'][0,:,:], cmap = 'gray')
+#plt.show()
+#time.sleep(60)
+
+train_loader = DataLoader(dataset_train,batch_size=5,shuffle=True) #possible num_workers = ?
+
+dataset_val = Dat.MiceDataset(csv_file=names,root_dir=root_dir,transform=transforms.Compose([Dat.RandomCrop((224,224)),Dat.RandomFlipHorizontal(),Dat.ToTensor()]))
+
+#sample = dataset_val[3]
+#print(3, sample['image'].type(), sample['gtruths'].type())
+#time.sleep(60)
+
+val_loader = DataLoader(dataset_val,batch_size=5,shuffle=True)
+
+#vgg = models.vgg16(pretrained=True)
+fcn_model = fcn.FCN8s(n_class) 
+#time.sleep(60)
+
+if use_gpu:
+	ts = time.time()
+	fcn_model = fcn_model.cuda()
+	print("Fnished cuda loading, time elapsed {}".format(time.time()-ts))
+
+criterion = nn.BCEWithLogitsLoss()
+#optimiser = optim.RMSprop(fcn_model.parameters(), lr=lr, momentum=momentum, weight_decay=w_decay)
+optimiser = optim.Adam(fcn_model.parameters(),lr=lr,betas=(0.9,0.999),eps=1e-8,weight_decay=w_decay)
+#optimiser = optim.SGD(fcn_model.parameters(), lr=lr, momentum=momentum, weight_decay=w_decay)
+scheduler = lr_scheduler.StepLR(optimiser, step_size=step_size,gamma=gamma)
+
+#create directory for saving the score
+
+def train():
+	for epoch in range(epochs):
+		scheduler.step()
+		
+		ts = time.time()
+		for iter,batch in enumerate(train_loader):
+			optimiser.zero_grad()
+			
+			if use_gpu:
+				inputs = batch['image'].float()
+				labels = batch['gtruths'].float()
+				inputs = Variable(inputs.cuda())
+				labels = Variable(labels.cuda())
+			else:
+				inputs,labels = Variable(batch['image']),Variable(batch['gtruths'])
+				
+			outputs = fcn_model(inputs)
+			loss = criterion(outputs,labels)
+			loss.backward()
+			summ = loss
+			optimiser.step()
+			
+			print ("epoch {}, iter {}, loss: {}".format(epoch, iter, loss.data[0]))
+		
+		log_value('Training Loss', summ, epoch)
+		print("Finish epoch {}, time elapsed {}".format(epoch, time.time() - ts))
+		
+		#torch.save(fcn_model,model_path)
+		
+		if epoch%5==0:
+			val(epoch)
+		
+		if epoch==2500 or epoch==5000:
+			model_path = os.path.join(model_dir,"epoch {}/".format(epoch))
+			if not os.path.exists(model_path):
+				os.makedirs(model_path)
+			torch.save(fcn_model,model_path+"epoch_{}".format(epoch))
+		
+		
+def val(epoch):
+	fcn_model.eval()
+	pixel_accs = []
+	for iter,batch in enumerate(val_loader):
+		if use_gpu:
+			inputs = batch['image'].float()
+			labels = batch['gtruths'].float()
+			inputs = Variable(inputs.cuda())
+			labels = Variable(labels.cuda())
+		else:
+			inputs,labels = Variable(batch['image']),Variable(batch['gtruths'])
+		
+		outputs = fcn_model(inputs)
+		val_loss = criterion(outputs,labels)
+		val_summ = val_loss.data[0]
+		
+	print ("epoch {}, validation_loss: {}".format(epoch, val_summ))
+		
+	log_value("Validation Loss", val_summ, epoch)
+	
+	i = np.random.randint(0,44)
+	sample = dataset_val[i]
+
+	inp = sample['image']
+	targ = sample['gtruths'].cpu().numpy()
+	#print(inp.size())
+	#time.sleep(3)
+	inp = inp.unsqueeze(0)
+	inp = Variable(inp.float().cuda())
+	o = fcn_model(inp)
+	sig = nn.Sigmoid()
+	o = sig(o)
+	o = o.data.cpu().numpy()
+	#print(o.shape)
+	#time.sleep(20)
+	for j in range(o.shape[1]):
+		a = o[0,j,:,:]
+		t = targ[j,:,:]
+		#print(a.max())
+		#time.sleep(1)
+		a = a*255
+		t = t*255
+		b = a.astype(np.uint8)
+		t = t.astype(np.uint8)
+		#print(b.dtype)
+		#time.sleep(60)
+		im = Image.fromarray(b)
+		im.save('/home/sridharg/Documents/Biomed_Imaging/images/epoch_{}_layer_{}_o_img.png'.format(epoch,j))
+		tim = Image.fromarray(t)
+		tim.save('/home/sridharg/Documents/Biomed_Imaging/images/epoch_{}_layer_{}_target_img.png'.format(epoch,j))
+
+
+
+if __name__ == "__main__":
+	val(0)
+	train()
+		
+	
+	
+	
+	
+		
+
+	
+
