2f3ff23eef306eb76551fa63a3ff3800c5dc0720
Gautam Sridhar <gautam.sridhar14@gmail.com>
2018-05-03 14:47:41 +0200

    Added batch normalization  layers

--- FCN8s_wVGG_mk3/FCN_VGG.py
+++ FCN8s_wVGG_mk3/FCN_VGG.py
@@ -39,112 +39,132 @@ class FCN8s(nn.Module):
 		#conv1
 		self.conv1_1 = nn.Conv2d(3,64,3,padding=100)
 		self.relu1_1 = nn.ReLU(inplace=True)
+		self.bn1_1 = nn.BatchNorm2d(64)
 		self.conv1_2 = nn.Conv2d(64,64,3,padding = 1)
 		self.relu1_2 = nn.ReLU(inplace=True)
+		self.bn1_2 = nn.BatchNorm2d(64)
 		self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True) #1/2 dimension reduction
 		
 		#conv2
 		self.conv2_1 = nn.Conv2d(64,128,3,padding = 1)
 		self.relu2_1 = nn.ReLU(inplace=True)
+		self.bn2_1 = nn.BatchNorm2d(128)
 		self.conv2_2 = nn.Conv2d(128,128,3,padding=1)
 		self.relu2_2 = nn.ReLU(inplace=True)
+		self.bn2_2 = nn.BatchNorm2d(128)
 		self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True) # 1/4 dimension reduction 
 		
 		
 		#conv3 
 		self.conv3_1 = nn.Conv2d(128,256,3,padding=1)
 		self.relu3_1 = nn.ReLU(inplace = True)
+		self.bn3_1 = nn.BatchNorm2d(256)
 		self.conv3_2 = nn.Conv2d(256,256,3,padding=1)
 		self.relu3_2 = nn.ReLU(inplace = True)
+		self.bn3_2 = nn.BatchNorm2d(256)
 		self.conv3_3 = nn.Conv2d(256,256,3,padding=1)
 		self.relu3_3 = nn.ReLU(inplace = True)
+		self.bn3_3 = nn.BatchNorm2d(256)
 		self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/8 dimension reduction
 		
 		#conv4
 		self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
 		self.relu4_1 = nn.ReLU(inplace=True)
+		self.bn4_1 = nn.BatchNorm2d(512)
 		self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
 		self.relu4_2 = nn.ReLU(inplace=True)
+		self.bn4_2 = nn.BatchNorm2d(512)
 		self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
 		self.relu4_3 = nn.ReLU(inplace=True)
+		self.bn4_3 = nn.BatchNorm2d(512)
 		self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True) # 1/16 dimension reduction
 		
 		#conv5
 		self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
 		self.relu5_1 = nn.ReLU(inplace=True)
+		self.bn5_1 = nn.BatchNorm2d(512)
 		self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
 		self.relu5_2 = nn.ReLU(inplace=True)
+		self.bn5_2 = nn.BatchNorm2d(512)
 		self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
 		self.relu5_3 = nn.ReLU(inplace=True)
+		self.bn5_3 = nn.BatchNorm2d(512)
 		self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True) # 1/32 dimension reduction
 		
 		#fc6
 		self.fc6 = nn.Conv2d(512,4096,7)
 		self.relu6 = nn.ReLU(inplace=True)
+		self.bn6 = nn.BatchNorm2d(4096)
 		self.drop6 = nn.Dropout2d()
 		
 		#fc7
 		self.fc7 = nn.Conv2d(4096,4096,1)
 		self.relu7 = nn.ReLU(inplace = True)
+		self.bn7 = nn.BatchNorm2d(4096)
 		self.drop7 = nn.Dropout2d()
 		
 		self.score_fr = nn.Conv2d(4096,n_class,1)             #Skip Layer defintions
+		self.bn_fr = nn.BatchNorm2d(n_class)
 		self.score_pool3 = nn.Conv2d(256,n_class,1)
+		self.bn_pool3 = nn.BatchNorm2d(n_class)
 		self.score_pool4 = nn.Conv2d(512,n_class,1)
+		self.bn_pool4 = nn.BatchNorm2d(n_class)
 		
 		self.upscore2 = nn.ConvTranspose2d(n_class,n_class,4,stride=2,bias=False)     #Upsampling layer defintions
+		self.bn_upscore2 = nn.BatchNorm2d(n_class)
 		self.upscore8 = nn.ConvTranspose2d(n_class,n_class,16,stride=8,bias=False)
 		self.upscore_pool4 = nn.ConvTranspose2d(n_class,n_class,4,stride=2,bias=False)
+		self.bn_upscore_pool4 = nn.BatchNorm2d(n_class)
 		
 		self._copy_params_from_vgg16()
 									
 	def forward(self,x):
 		h = x
-		h = self.relu1_1(self.conv1_1(h))
-		h = self.relu1_2(self.conv1_2(h))
+		h = self.bn1_1(self.relu1_1(self.conv1_1(h)))
+		h = self.bn1_2(self.relu1_2(self.conv1_2(h)))
 		h = self.pool1(h)
 			
-		h = self.relu2_1(self.conv2_1(h))
-		h = self.relu2_2(self.conv2_2(h))
+		h = self.bn2_1(self.relu2_1(self.conv2_1(h)))
+		h = self.bn2_2(self.relu2_2(self.conv2_2(h)))
 		h = self.pool2(h)
 			
-		h = self.relu3_1(self.conv3_1(h))
-		h = self.relu3_2(self.conv3_2(h))
-		h = self.relu3_3(self.conv3_3(h))
+		h = self.bn3_1(self.relu3_1(self.conv3_1(h)))
+		h = self.bn3_2(self.relu3_2(self.conv3_2(h)))
+		h = self.bn3_3(self.relu3_3(self.conv3_3(h)))
 		h = self.pool3(h)
 		pool3 = h # 1/8
 			
-		h = self.relu4_1(self.conv4_1(h))
-		h = self.relu4_2(self.conv4_2(h))
-		h = self.relu4_3(self.conv4_3(h))
+		h = self.bn4_1(self.relu4_1(self.conv4_1(h)))
+		h = self.bn4_2(self.relu4_2(self.conv4_2(h)))
+		h = self.bn4_3(self.relu4_3(self.conv4_3(h)))
 		h = self.pool4(h)
 		pool4 = h # 1/16
 			
-		h = self.relu5_1(self.conv5_1(h))
-		h = self.relu5_2(self.conv5_2(h))
-		h = self.relu5_3(self.conv5_3(h))
+		h = self.bn5_1(self.relu5_1(self.conv5_1(h)))
+		h = self.bn5_2(self.relu5_2(self.conv5_2(h)))
+		h = self.bn5_3(self.relu5_3(self.conv5_3(h)))
 		h = self.pool5(h)
 		
 			
-		h = self.relu6(self.fc6(h))
+		h = self.bn6(self.relu6(self.fc6(h)))
 		h = self.drop6(h)
 			
-		h = self.relu7(self.fc7(h))
+		h = self.bn7(self.relu7(self.fc7(h)))
 		h = self.drop7(h)
 			
-		h = self.score_fr(h)
-		h = self.upscore2(h)
+		h = self.bn_fr(self.score_fr(h))
+		h = self.bn_upscore2(self.upscore2(h))
 		upscore2 = h  # 1/16
 			
-		h = self.score_pool4(pool4)
+		h = self.bn_pool4(self.score_pool4(pool4))
 		h = h[:, :, 5:5 + upscore2.size()[2], 5:5 + upscore2.size()[3]]
 		score_pool4c = h
 			
 		h = upscore2 + score_pool4c # 1/16
-		h = self.upscore_pool4(h)
+		h = self.bn_upscore_pool4(self.upscore_pool4(h))
 		upscore_pool4 = h # 1/8
 			
-		h = self.score_pool3(pool3)
+		h = self.bn_pool3(self.score_pool3(pool3))
 		h = h[:,:,9:9+upscore_pool4.size()[2],9:9+upscore_pool4.size()[3]]
 		score_pool3c = h
 			
